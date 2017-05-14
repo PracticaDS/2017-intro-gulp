7e28166fafadb2d2fffff33c605d176ee7826ebc
Javier Fernandes <javier.fernandes@gmail.com>
2017-05-14 12:37:20 -0300

    variantes de definición de tareas

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,11 @@
 import gulp from 'gulp'
 import gutil from 'gulp-util'
 
-gulp.task('default', () => {
-  return gutil.log('Gulp is running!')
-})
+// gulp.task()
+gulp.task('beep', () => gutil.beep())
+gulp.task('hola', () => gutil.log('Hola'))
+gulp.task('chau', ['beep'], () => gutil.log('Chau'))
+
+gulp.task('default', ['hola', 'chau'])
+
+
